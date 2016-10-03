79ba226a5a64bfdfbd485e9b1c25d014ac49ad05
BautiG <juanbautistagallino@hanovernorwichschools.org>
2016-10-03 13:51:31 -0400

    Updated from Brython Server: 10/3/2016 1:51:31 PM

--- tutotial3.py
+++ tutotial3.py
@@ -19,5 +19,28 @@ ball.scale = 0.1
 ball.dir = 1
 ball.go = True
 
+def reverse(b):
+    b.dir *= -1
+    
+def step():
+    if ball.go:
+        ball.x += ball.dir
+        if ball.x + ball.width > SCREEN_WIDTH or ball.x < 0:
+            ball.x -= ball.dir
+            reverse(ball)
+
+# Handle the space key
+def spaceKey(event):
+    ball.go = not ball.go
+
+# Handle the "reverse" key
+def reverseKey(event):
+    reverse(ball)
+
+# Handle the mouse click
+def mouseClick(event):
+    ball.x = event.x
+    ball.y = event.y
+
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
-myapp.run()
+myapp.run(step)

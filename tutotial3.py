a48663c8ebca005dcf50220567b4ab9b40a2664e
BautiG <juanbautistagallino@hanovernorwichschools.org>
2016-10-04 13:18:52 -0400

    Updated from Brython Server: 10/4/2016 1:18:53 PM

--- tutotial3.py
+++ tutotial3.py
@@ -1,27 +1,24 @@
-from ggame import App, RectangleAsset, ImageAsset, SoundAsset
-from ggame import LineStyle, Color, Sprite, Sound
-
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
-
-green = Color(0x00ff00, 1)
-black = Color(0, 1)
-
-noline = LineStyle(0, black)
-bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, green)
-bg = Sprite(bg_asset, (0,0))
-
+# A ball! This is already in the ggame-tutorials repository
 ball_asset = ImageAsset("images/orb-150545_640.png")
 ball = Sprite(ball_asset, (0, 0))
 # Original image is too big. Scale it to 1/10 its original size
 ball.scale = 0.1
+ball.y = 200
 # custom attributes
 ball.dir = 1
 ball.go = True
+# Sounds
+pew1_asset = SoundAsset("sounds/pew1.mp3")
+pew1 = Sound(pew1_asset)
+pop_asset = SoundAsset("sounds/reappear.mp3")
+pop = Sound(pop_asset)
+
 
 def reverse(b):
     b.dir *= -1
-    
+    pop.play()
+
+# Set up function for handling screen refresh
 def step():
     if ball.go:
         ball.x += ball.dir
@@ -41,9 +38,9 @@ def reverseKey(event):
 def mouseClick(event):
     ball.x = event.x
     ball.y = event.y
+    pew1.play()
 
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
-
 # Set up event handlers for the app
 myapp.listenKeyEvent('keydown', 'space', spaceKey)
 myapp.listenKeyEvent('keydown', 'r', reverseKey)

21e8e8919afbe16d372e71d5b1ca3eeb7aa4864b
BautiG <juanbautistagallino@hanovernorwichschools.org>
2016-10-28 13:32:50 -0400

    Updated from Brython Server: 10/28/2016 1:32:50 PM

--- tutotial3.py
+++ tutotial3.py
@@ -1,3 +1,20 @@
+from ggame import App, RectangleAsset, ImageAsset, SoundAsset, Sprite, Sound
+from ggame import LineStyle, Color
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+green = Color(0x00ff00, 1)
+black = Color(0, 1)
+noline = LineStyle(0, black)
+bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, green)
+bg = Sprite(bg_asset, (0,0))
+
+# Sounds
+pew1_asset = SoundAsset("sounds/pew1.mp3")
+pew1 = Sound(pew1_asset)
+pop_asset = SoundAsset("sounds/reappear.mp3")
+pop = Sound(pop_asset)
 # A ball! This is already in the ggame-tutorials repository
 ball_asset = ImageAsset("images/orb-150545_640.png")
 ball = Sprite(ball_asset, (0, 0))
